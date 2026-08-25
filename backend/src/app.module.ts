bab8260d1cbc6c1abc57c11661ec057b939b2c03
TrungSy2106 <dinhcongtrungduc@gmail.com>
2026-08-25 17:45:18 +0700

    chore(backend): tracking modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { EventsModule } from './modules/events/events.module';
+import { ArticlesModule } from './modules/articles/articles.module';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { WebsitesModule } from './modules/websites/websites.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
+    EventsModule,
+    ArticlesModule,
+    SessionsModule,
+    WebsitesModule,
+    DashboardModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
